936e7a4396d4e28c32b0117b365cbb634721539d
chamundeswari1212 <vscchaithanya1234@gmail.com>
2025-10-09 19:26:50 +0530

    python basics

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,32 @@
+a=10
+b=20
+print(a+b)
+greetings="good morning"
+print(greetings)
+print("hello world")
+
+import keyword
+print(keyword.kwlist)
+
+#Identifiers
+data="hello"
+print(data)
+#2daydata (it is not valid, as it starts with digit)
+_2daydate="hello"
+
+a=20
+A=30
+
+
+#class = "some data"-> keywords not allowed
+
+#define some student data
+#the way of using identifiers
+x="Ravi"
+y=25
+z="python Course"
+
+
+student_firstname="ravi"
+student_age=25
+student_course="python course"
