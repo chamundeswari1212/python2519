5e181d84a3d935fe91fc9059eab28a48599680e3
chamundeswari1212 <vscchaithanya1234@gmail.com>
2025-10-09 11:51:22 +0530

    sample codes basics

--- classes/demo.py
+++ classes/demo.py
@@ -7,3 +7,26 @@ print("hello world")
 
 import keyword
 print(keyword.kwlist)
+
+#Identifiers
+data="hello"
+print(data)
+#2daydata (it is not valid, as it starts with digit)
+_2daydate="hello"
+
+a=20
+A=30
+
+
+#class = "some data"-> keywords not allowed
+
+#define some student data
+#the way of using identifiers
+x="Ravi"
+y=25
+z="python Course"
+
+
+student_firstname="ravi"
+student_age=25
+student_course="python course"
