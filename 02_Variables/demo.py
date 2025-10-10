f986508b90e1874b875a9036e7a0479ddea4ce60
chamundeswari1212 <vscchaithanya1234@gmail.com>
2025-10-10 18:36:08 +0530

    membership, identity operators

--- 02_Variables/demo.py
+++ 02_Variables/demo.py
@@ -23,6 +23,8 @@ print(id(b))
 c=10
 print(id(c))
 
+
+print("="*20)
 #List means collection of multiple items --> list is represented using []
 a_list = [10,20,30]
 print(type(a_list))
@@ -65,6 +67,8 @@ print(x+y+z)
 s1,s2,s3 = "orange","apple","cherry"
 print(s1+s2+s3)
 
+
+print("="*20)
 # using concatenation
 name = "ravi"
 age = 25
@@ -74,12 +78,15 @@ age = 25
 # using interpolation
 print(f"My name is {name} and im {age} years old")
 
+
+print("="*20)
 # using different values dynamically
 x,y,z=10,20,30
 age=25
 print(f"Sum of x,y,z,age:{x+y+z+age}")
 
 
+print("="*20)
 # school student info
 name = "john"
 student_class = 8
@@ -87,6 +94,7 @@ student_school = "DPS"
 print(f"My name is {name} is studying {student_class} class in {student_school} school")
 
 
+print("="*20)
 # key specifications of Mahindra Scorpio
 ARAI_Mileage = "14.44 kmpl"
 Engine_Displacement="2184 cc"
@@ -95,3 +103,82 @@ Seating_capacity=7.9
 Boot_Space="460 Litres"
 Body_Type="SUV"
 print(f"key specifications of Mahindra Scorpio:ARAI_Mileage: {ARAI_Mileage} and Engine_Displacement: {Engine_Displacement} and Max_Power: {Max_Power} and Seating_capacity: {Seating_capacity} and Boot_Space: {Boot_Space} and Body_Type: {Body_Type}")
+
+print("="*20)
+# Operators
+
+# Arithmetic Operator:
+n1=3
+n2=2
+print(f"Sum of n1 and n2 is: {n1+n2}")
+print(f"Difference of n1 and n2: {n1-n2}")
+print(f"Product of n1 and n2: {n1*n2}")
+print(f"Division of n1 and n2: {n1/n2}")
+print(f"Modules of n1 and n2: {n1%n2}")
+print(f"Floor Division of n1 and n2: {n1//n2}")
+print(f"Exponentiation of n1 and n2: {n1**n2}")
+
+
+print("="*20)
+# Compound Assignment Operators
+#without compound Assignment operator
+x=10
+x=x+5
+print(x)
+#with compound Assignment operator
+x=10
+x += 5
+print(x)
+
+print("="*20)
+# Comparision Operators
+n1=3
+n2=2
+print(n1==n2)
+
+print("="*20)
+# Logical Operators
+x=7
+y=5
+a=5
+b=9
+resultand= a>y and a<b # T and F --> F
+print(resultand)
+resultor= a>y or a<b # T or F --> T
+print(resultor)
+resultnot= a>y or a<b # T or F --> T --> not (T) --> F
+print(not resultnot)
+
+
+print("="*20)
+# Membership operators
+a_list = [10,20,30]
+is_present = 10 in a_list
+print(is_present)
+is_present = 100 in a_list
+print(is_present)
+is_present = 100 not in a_list
+print(is_present)
+
+
+data = "hello"
+is_present = "l" in data
+print(is_present)
+
+print("="*20)
+# Identity Operator
+n1 = 10
+n2 = 10
+print(n1 is n2)
+print(id(n1))
+print(id(n2))
+
+n1 = [10,20,30]
+n2 = [10,20,30]
+print(n1 is n2)
+print(n1 == n2)
+print(n1 is not n2)
+print(id(n1))
+print(id(n2))
+
+
