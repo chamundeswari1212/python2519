90383f95ad097d38ca6065fdec1ebc7cf79a8937
chamundeswari1212 <vscchaithanya1234@gmail.com>
2025-10-13 19:27:47 +0530

    converstion, casting explicit, implicit

--- 02_Variables/demo.py
+++ 02_Variables/demo.py
@@ -266,4 +266,37 @@ print(f"Student Current Attendance : {Student_attendence}")
 print(f"Student Passed : {student_passed}")
 print(f"Student Awarded : {award_eligibility}")
 
+# Type conversion -> Automatic / Implicit
+a = 10
+b = 3.5
+c = a + b #13.5
+print(c)
+print(type(c))
 
+
+# Type Casting -> Manual / Explicit
+x = 100
+y = x
+print(y)
+print(type(y))
+
+# explicit conversion
+y = float(x)
+print(y)
+print(type(y))
+
+# Data loss can occur
+r = 3.14
+value = int(r)
+print(value)
+
+# strings
+value = "100"
+print(type(value))
+# print(value/10) # TypeError : unsupported operand type(s) for /: 'str' and 'int'
+value_int = int(value)
+print(value_int/10)
+
+value = "hundred"
+# value_int = int(value)
+# print(value_int) # ValueError: invalid literal for int() with base 10: 'hundred'
