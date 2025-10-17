9fffa96c7baad9b454266367eb4f502cba7ecb0c
chamundeswari1212 <vscchaithanya1234@gmail.com>
2025-10-17 18:20:33 +0530

    nested loops and branching statements

--- 03_control_statements/looping.py
+++ 03_control_statements/looping.py
@@ -64,7 +64,93 @@ print("="*50)
 for i in range(10):
     print("Hi")
 
+print("="*50)
+for i in range (1,6):
+    print(i)
+
+print("="*50)
+for num in range (1,10,1):
+    print(num)
+
+print("="*50)
+for ij in range(1,10,2):
+    print(ij)
+
+print("="*50)
+for i in range(10,1,-1):
+    print(i)
+
+# for i in range(1,10,1,4): # TypeError : range expected at most 3 arguments, got 4
+    # print(i) 
+
+print("="*50)
+
+# get even numbers
+print("printing even nums from 1 to 20")
+num =2
+while num <=20:
+    print(num)
+    num +=2
+
+# loop with condition
+num =2
+while num <= 20:
+    if num%2 ==0:
+        print(num)
+    num+= 1
+
+print("="*50)
+# get even numbers
+print("printing even numbers 1 to 20:")
+for i in range(2,22,2):
+    print(i)
+    
 
 
+# List of courses
+course_list = ["python","cloud","devops","ai"]
+for course in course_list:
+    print(course)
 
 
+# Nested loops
+for i in range(1,4):
+    for j in range(1,4):
+        print(f"{i} X {j} = {i*j}")
+    print("------")
+    
+# Nested loops
+i = 1
+while i<4:
+    j=1
+    while j<4:
+        print(f"{i} X {j} = {i*j}")
+        j+=1
+    print("-------")
+    i+=1
+    
+# Branching statements
+# break
+
+for i in range(5):
+    if i ==3:
+        break # loop will stop here
+    print(i)
+
+# continue: skips the current iteration and continue the loop
+for i in range(5):
+    if i==3:
+        continue # skip this current iteration
+    print(i)
+
+# pass: does nothing, generally used as place holder 
+if 5>9:
+    pass
+
+
+print("="*25)
+# pass : using for future
+for num in range(5):
+    if num==3:
+        pass #keep future code here
+    print(num)
